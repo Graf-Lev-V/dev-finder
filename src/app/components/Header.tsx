55f0babd030b9ce55aa26966dfca327d43f3967e
Graf-Lev-V <legendarlev2004@gmail.com>
2026-05-16 01:32:54 +0300

    update

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
         <header className="bg-gray-900 p-4 text-white">
             <nav className="flex gap-4">
                 <Link href='/'>Home</Link>
-                <Link href='https://github.com/Graf-Lev-V/dev-finder'>Repository</Link>
+                <Link href='https://github.com/Graf-Lev-V/dev-finder' target="_blank">Repository</Link>
             </nav>
         </header>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,23 @@ import { useState } from "react";
 export default function Home() {
 
   const router = useRouter();
-
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState('');
 
   return (
-    <div>
-      <h1 className="text-xl">Dev Finder</h1>
-      <input value={search} onChange={(e) => setSearch(e.target.value)}/>
-      <button onClick={() => router.push(`/users/?q=${search}`)}>Search</button>
+    <div className="flex flex-col items-center justify-center flex-1 pb-32">
+      <h1 className="text-5xl font-bold text-gray-900 mb-4">Dev Finder</h1>
+      <p className="text-gray-700 text-base mb-8">Search GitHub users by username.</p>
+      <div className="flex gap-2">
+        <input 
+          className="border border-gray-300 py-3 px-4 rounded-md w-80 focus:outline-offset-2 focus:outline-2 outline-blue-500" 
+          value={search} 
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button 
+          className="bg-blue-600 text-white py-3 px-6 rounded-md hover:bg-blue-800 hover:cursor-pointer" 
+          onClick={() => router.push(`/users/?q=${search}`)}
+        >Search</button>
+      </div>
     </div>
   );
 }
