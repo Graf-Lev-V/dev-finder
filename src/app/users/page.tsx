69426e62ddf90c9ada4f07ac88d6d179c19dc3c8
Graf-Lev-V <legendarlev2004@gmail.com>
2026-05-16 17:55:49 +0300

    fix

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,56 +1,10 @@
-'use client'
-
-import { useSearchParams } from "next/navigation";
-export const dynamic = 'force-dynamic';
-
-import { User } from '../types';
-import { useEffect, useState } from "react";
-import UserCard from "../components/UserCard";
+import { Suspense } from "react";
+import UsersContent from '../components/UsersContent';
 
 export default function Users() {
-
-    const searchParams = useSearchParams();
-    const query = searchParams.get('q');
-
-    const [users, setUsers] = useState<User[]>([]);
-    const [loading, setLoading] = useState<boolean>(false);
-    const [error, setError] = useState<Error | null>(null);
-
-    useEffect(() => {
-        const controller = new AbortController();
-        (async () => {
-            setUsers([]);
-            setError(null);
-            setLoading(true);
-            try {
-                const response = await fetch(`https://api.github.com/search/users?q=${query}`, {signal: controller.signal});
-                if (!response.ok) throw new Error('Error');
-                const data = await response.json();
-                setUsers(data.items);
-            }
-            catch (error) {
-                if (error instanceof Error && error.name !== "AbortError") {
-                    setError(error);
-                }
-            }
-            finally {
-                if (!controller.signal.aborted) {
-                    setLoading(false);
-                }
-            }
-        })();
-
-        return () => controller.abort();
-    }, [query])
-
     return (
-        <div className="p-6">
-            <h2 className="text-lg font-bold mb-4">Results for: &quot;{query}&quot;</h2>
-            {loading && <p>Loading...</p>}
-            {error && <p>{error.message}</p>}
-            <div className="grid grid-cols-3 gap-4">
-                {users.map((user) => <UserCard key={user.id} login={user.login} avatar={user.avatar_url}/>)}
-            </div>
-        </div>
+        <Suspense fallback={<p>Loading...</p>}>
+            <UsersContent/>
+        </Suspense>
     )
 }
